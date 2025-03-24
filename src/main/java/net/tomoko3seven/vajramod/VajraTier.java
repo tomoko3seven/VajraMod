269a6ff75091a3c02f2009e6cec938b76e182733
tomoko3seven <sqvizers@gmail.com>
2025-03-25 00:47:48 +0100

    Main

---
+++ src/main/java/net/tomoko3seven/vajramod/VajraTier.java
@@ -0,0 +1,37 @@
+package net.tomoko3seven.vajramod;
+
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.block.Blocks;
+
+public class VajraTier implements Tier {
+    @Override
+    public int getLevel() {
+        return 1000; // Позволяет ломать всё
+    }
+
+    @Override
+    public int getUses() {
+        return Integer.MAX_VALUE; // Бесконечная прочность
+    }
+
+    @Override
+    public float getSpeed() {
+        return 100.0f; // Супер-быстрая добыча
+    }
+
+    @Override
+    public float getAttackDamageBonus() {
+        return 10.0f;
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return 100;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.of(Blocks.DIAMOND_BLOCK); // Можно настроить на что угодно
+    }
+}
